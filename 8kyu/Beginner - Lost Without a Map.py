97a41c526ba9c222752f1eb682a27890b7e8b0c3
Marco0201 <marcoperz0201@gmail.com>
2022-10-23 20:21:54 -0700

    new kata solution

---
+++ 8kyu/Beginner - Lost Without a Map.py
@@ -0,0 +1,11 @@
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+def maps(a):
+    num = []
+    for i in a:
+        num.append(i * 2)
+    return num
