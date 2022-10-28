8f209d58110016a5a05523ad1e065ded40aef06c
Marco0201 <marcoperz0201@gmail.com>
2022-10-28 14:30:05 -0700

    new kata solution

---
+++ 7kyu/Multiply Word in String.py
@@ -0,0 +1,20 @@
+
+
+#     You are to write a function that takes a string as its first parameter. This string will be a string of words.
+
+#     You are expected to then use the second parameter, which will be an integer, to find the corresponding word in the given string. The first word would be represented by 0.
+
+#     Once you have the located string you are finally going to multiply by it the third provided parameter, which will also be an integer. You are additionally required to add a hyphen in between each word.
+
+# Example
+
+# modify_multiply ("This is a string",3,5)
+
+def modify_multiply(st, loc, num):
+    a = st.split()
+    l = 0
+    word = []
+    while l <= num - 1:
+        word.append(a[loc])
+        l += 1
+    return '-'.join(word)
