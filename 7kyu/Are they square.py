82906f84127e655f3981e5911a409773644b7984
Marco0201 <marcoperz0201@gmail.com>
2022-11-07 17:43:45 -0800

    new kata solution

---
+++ 7kyu/Are they square.py
@@ -0,0 +1,30 @@
+# Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+# Your function should return true if all elements in the array are square numbers and false if not.
+
+# An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+# Examples:
+
+# is_square([1, 4, 9, 16]) --> True
+
+# is_square([3, 4, 7, 9]) --> False
+
+# is_square([]) --> None
+
+import math
+
+
+def is_square(arr):
+    l = 0
+    num = 0
+    if not arr:
+        return None
+    else:
+        while l <= len(arr) - 1:
+            if math.sqrt(arr[l]) == int(arr[l] ** 0.5):
+                l += 1
+                num += 1
+            else:
+                l += 1
+        return num == len(arr)
