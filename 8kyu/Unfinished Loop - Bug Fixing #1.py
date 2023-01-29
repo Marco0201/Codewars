0d7ad5600135132074a682d31ff9ef9fd4768ca2
Marco0201 <marcoperz0201@gmail.com>
2023-01-28 21:13:56 -0800

    changed variable

--- 8kyu/Unfinished Loop - Bug Fixing #1.py
+++ 8kyu/Unfinished Loop - Bug Fixing #1.py
@@ -1,8 +1,8 @@
 # Unfinished Loop - Bug Fixing #1
 
 # Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
-def create_array(n):
+def create_array(a):
     res = []
-    for i in range(1, n+1):
+    for i in range(1, a+1):
         res.append(i)
     return res
