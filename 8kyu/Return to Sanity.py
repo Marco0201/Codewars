0f9d1c147cc31212db025c4ec11d187418525d4e
Marco0201 <marcoperz0201@gmail.com>
2023-02-06 22:36:45 -0800

    refactored code

--- 8kyu/Return to Sanity.py
+++ 8kyu/Return to Sanity.py
@@ -1,4 +1,3 @@
 # This function should return an object, but it's not doing what's intended. What's wrong?
 def mystery():
-    results = {'sanity': 'Hello'}
-    return results
+    return {'sanity': 'Hello'}
