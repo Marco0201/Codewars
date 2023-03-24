a42a97489532572d0df0a6f038bda212fa7f101d
Marco0201 <marcoperz0201@gmail.com>
2023-03-24 16:12:14 -0700

    added solution

--- python/7kyu/Divide and Conquer.py
+++ python/7kyu/Divide and Conquer.py
@@ -1,3 +1,11 @@
-Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+# Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
 
-Return as a number.
+# Return as a number.
+def div_con(x):
+    total = 0
+    for i in x:
+        if type(i) == type(0):
+            total += i
+        else:
+            total -= int(i)
+    return total
