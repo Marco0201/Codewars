96e0ba321b975af49a87536075cafcb118fe7154
Marco0201 <marcoperz0201@gmail.com>
2023-03-28 23:07:51 -0700

    new kata

---
+++ javascript/7kyu/Decreasing Inputs.js
@@ -0,0 +1,35 @@
+// This kata is all about adding numbers.
+
+// You will create a function named add. It will return the sum of all the arguments. Sounds easy, doesn't it?
+
+// Well Here's the Twist. The inputs will gradually decrease with their index as parameter to the function.
+
+//   add(3,4,6); 
+//   /*
+//   returns ( 3 / 1 ) + ( 4 / 2 ) + ( 6 / 3 ) = 7
+//   */
+
+// Remember the function will return 0 if no arguments are passed and it must round the result if sum is a float.
+
+// Example
+
+//   add(); //=> 0
+//   add(1,2,3); //=> 3
+//   add(1,4,-6,20); //=> 6
+
+// Check my another kata here!! http://www.codewars.com/kata/555b73a81a6285b6ce000047
+
+function add() {
+  let total = 0
+  let i = 1
+  if (arguments === null) {
+    return 0
+  } else {
+      for(let arr of arguments) {
+        total += (arr / i);
+        i++;
+    }
+        
+  }
+  return Math.round(total)
+}
