772d12018a590e09e8ae324d3ea1d4c1eca160dc
Marco0201 <marcoperz0201@gmail.com>
2023-04-01 21:53:25 -0700

    new kata added

---
+++ python/8kyu/Reversed Strings.py
@@ -0,0 +1,10 @@
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# Strings
+# Fundamentals
+
+def solution(str):
+    return str[::-1]
