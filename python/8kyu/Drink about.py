11cfed1b13fec6840b8db3acaca5dcb093c9e301
Marco0201 <marcoperz0201@gmail.com>
2023-05-04 20:44:06 -0700

    added new codewars solution

--- python/8kyu/Drink about.py
+++ python/8kyu/Drink about.py
@@ -20,3 +20,12 @@
 # 18 --> "drink beer"
 # 20 --> "drink beer"
 # 30 --> "drink whisky"
+
+def people_with_age_drink(age):
+    if age > 20:
+        return 'drink whisky'
+    if age > 17:
+        return 'drink beer'
+    if age > 13:
+        return 'drink coke'
+    return 'drink toddy'
