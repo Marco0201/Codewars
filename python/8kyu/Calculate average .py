7d047140f4f18df84db9ba572cee1e8c8df6d2cc
Marco0201 <marcoperz0201@gmail.com>
2023-05-14 15:40:32 -0700

    changed variable

--- python/8kyu/Calculate average .py
+++ python/8kyu/Calculate average .py
@@ -1,8 +1,8 @@
 # Write a function which calculates the average of the numbers in a given list.
 
 # Note: Empty arrays should return 0.
-def find_average(numbers):
-    if numbers:
-        return sum(numbers) / len(numbers)
+def find_average(num):
+    if num:
+        return sum(num) / len(num)
     else:
         return 0
