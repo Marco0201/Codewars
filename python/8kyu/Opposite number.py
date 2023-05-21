6ee11c7cceebd2bb686474483f54764641001f32
Marco0201 <marcoperz0201@gmail.com>
2023-05-21 16:18:07 -0700

    added new codewars solution

--- python/8kyu/Opposite number.py
+++ python/8kyu/Opposite number.py
@@ -5,3 +5,14 @@
 # 1: -1
 # 14: -14
 # -34: 34
+def opposite(number):
+    if number > 0 and type(number) == type(22.33):
+        return float('-' + str(number))
+    elif number > 0 and type(number) == type(2):
+        return int('-' + str(number))
+    elif number < 0 and type(number) == type(2.2):
+        return float(str(number).replace('-', ''))
+    elif number < 0 and type(number) == type(2):
+        return int(str(number).replace('-', ''))
+    else:
+        return 0
