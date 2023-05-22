155f3d8c3df4234d1b2fab98db170812843af8be
Marco0201 <marcoperz0201@gmail.com>
2023-05-22 16:18:58 -0700

    changed variable

--- python/8kyu/Opposite number.py
+++ python/8kyu/Opposite number.py
@@ -5,14 +5,14 @@
 # 1: -1
 # 14: -14
 # -34: 34
-def opposite(number):
-    if number > 0 and type(number) == type(22.33):
-        return float('-' + str(number))
-    elif number > 0 and type(number) == type(2):
-        return int('-' + str(number))
-    elif number < 0 and type(number) == type(2.2):
-        return float(str(number).replace('-', ''))
-    elif number < 0 and type(number) == type(2):
+def opposite(num):
+    if num > 0 and type(num) == type(22.33):
+        return float('-' + str(num))
+    elif num > 0 and type(num) == type(2):
+        return int('-' + str(num))
+    elif num < 0 and type(num) == type(2.2):
+        return float(str(num).replace('-', ''))
+    elif num < 0 and type(num) == type(2):
         return int(str(number).replace('-', ''))
     else:
         return 0
