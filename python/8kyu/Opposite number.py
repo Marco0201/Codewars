e7217e1f96ffb7d89560c71492dd92dabd067c1e
Marco0201 <marcoperz0201@gmail.com>
2023-05-23 16:19:25 -0700

    fixed variable name

--- python/8kyu/Opposite number.py
+++ python/8kyu/Opposite number.py
@@ -13,6 +13,6 @@ def opposite(num):
     elif num < 0 and type(num) == type(2.2):
         return float(str(num).replace('-', ''))
     elif num < 0 and type(num) == type(2):
-        return int(str(number).replace('-', ''))
+        return int(str(num).replace('-', ''))
     else:
         return 0
