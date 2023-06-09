eaa8df58bdff734b0be64278373376a5795d6ebb
Marco0201 <marcoperz0201@gmail.com>
2023-06-08 20:59:38 -0700

    added new codewars solution

--- javascript/8kyu/Find the position!.js
+++ javascript/8kyu/Find the position!.js
@@ -3,3 +3,8 @@
 // Input :: "a"
 
 // Ouput :: "Position of alphabet: 1"
+function position(letter){
+    let alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    let i = alph.indexOf(letter)
+    return `Position of alphabet: ${i +1}`
+  }
