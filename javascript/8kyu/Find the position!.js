a4aa86b773a39f616b8959665cfc689194091f8a
Marco0201 <marcoperz0201@gmail.com>
2023-06-10 21:00:30 -0700

    fixed variable name

--- javascript/8kyu/Find the position!.js
+++ javascript/8kyu/Find the position!.js
@@ -5,6 +5,6 @@
 // Ouput :: "Position of alphabet: 1"
 function position(l){
     let alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    let i = alph.indexOf(l)
-    return `Position of alphabet: ${i +1}`
+    // let i = alph.indexOf(l)
+    return `Position of alphabet: ${alph.indexOf(l)}`
   }
