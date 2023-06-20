32281e93e42a13a4e421add1740071a1c38d0a4e
Marco0201 <marcoperz0201@gmail.com>
2023-06-19 21:21:16 -0700

    added new codewars solution

--- javascript/8kyu/Drink about.js
+++ javascript/8kyu/Drink about.js
@@ -21,3 +21,14 @@
 // 20 --> "drink beer"
 // 30 --> "drink whisky"
 
+function peopleWithAgeDrink(old) {
+    if (old < 14) {
+      return 'drink toddy'
+    } else if (old > 14 && old < 18) {
+      return "drink coke"
+    } else if (old > 17 && old < 21) {
+      return "drink beer"
+    } else {
+      return "drink whisky"
+    }
+  };
