3e3d2f0e2c794d5f219493c7095a88c9fb7a8749
Marco0201 <marcoperz0201@gmail.com>
2023-06-26 18:58:48 -0700

    changed variable

--- javascript/8kyu/Sum of Multiples.js
+++ javascript/8kyu/Sum of Multiples.js
@@ -1,14 +1,14 @@
-Your Job
+// Your Job
 
-Find the sum of all multiples of n below m
-Keep in Mind
+// Find the sum of all multiples of n below m
+// Keep in Mind
 
-    n and m are natural numbers (positive integers)
-    m is excluded from the multiples
+//     n and m are natural numbers (positive integers)
+//     m is excluded from the multiples
 
-Examples
+// Examples
 
-sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
-sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
-sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
-sumMul(4, -7)  ==> "INVALID"
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
